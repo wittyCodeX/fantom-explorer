79d5bf592f5396f909e203549684f48a913d5525
ethancrypto <jonathan.cryptoguru@gmail.com>
2022-10-07 16:22:46 +0900

    updated HQ navbar image

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,34 +4,42 @@ import services from "services";
 
 export default function Footer(props) {
   return (
-    <footer className=" w-full flex justify-center px-2 py-4 bg-gray-200 text-white sm:px-4 shadow-xl dark:bg-[#2c2e3f] dark:border-gray-700 z-10">
+    <footer className=" w-full flex justify-center px-2 py-4 bg-[#0813ff] text-white sm:px-4 shadow-xl dark:bg-[#2c2e3f] dark:border-gray-700 z-10">
       {/* Bottom area */}
-      <div className="flex flex-row justify-between md:flex md:flex-row md:items-center md:justify-between py-1 border-t border-gray-300 dark:border-gray-500 w-screen max-w-6xl">
+      <div className="flex flex-row justify-between md:flex md:flex-row md:items-center md:justify-between py-1 border-t border-blue-400 dark:border-gray-500 w-screen max-w-6xl">
         {/* Copyrights note */}
-        <div className="flex text-sm text-gray-600 mr-4">
+        <div className="flex text-sm text-white mr-4">
           <div className="m-auto">
-            <a href="https://fantom.foundation">
-              <img
-                src={services.linking.static("images/fantom-ftm-logo.png")}
-                alt="Powered by Fantom."
-                className="inline-block object-scale-down w-10 h-10 m-2"
-              />
-              <span className="text-lg md:text-lg dark:text-white text-black">
-                Powered by Fantom
-              </span>
-            </a>
+            Fantom Explorer <span>&#169;</span>
+            {"   " + new Date().getFullYear()} - Developed by Fantom Foundation
           </div>
         </div>
         {/* Copyrights note */}
-        <div className="flex text-sm dark:text-gray-300 text-black mr-4">
-          <div className="m-auto">
-            Copyright <span>&#169;</span>
-            {"   " + new Date().getFullYear()} All rights reserved
-          </div>
-        </div>
-        <div className="flex text-sm text-gray-600 mr-4">
+        <div className="flex flex-row items-center text-sm text-gray-600 gap-4">
+          <a
+            className="py-2 cursor-pointer"
+            href="mailto:jonathan.cryptoguru@gmail.com"
+          >
+            <img
+              src={services.linking.static("images/telegram.svg")}
+              alt="mail"
+              srcSet=""
+              className="w-6"
+            />
+          </a>
+          <a
+            className="py-2 cursor-pointer"
+            href="mailto:jonathan.cryptoguru@gmail.com"
+          >
+            <img
+              src={services.linking.static("images/email.svg")}
+              alt="mail"
+              srcSet=""
+              className="w-6"
+            />
+          </a>
           <div
-            className="py-4 px-4 cursor-pointer"
+            className="py-2 cursor-pointer"
             onClick={() => props.handleDarkMode(!props.isDarkmode)}
           >
             {props.isDarkmode
